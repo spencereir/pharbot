bf15417b9e0dd0e399f3260f78744da9501cac05
Spencer Whitehead <spencer.whitehead@rogers.com>
2018-04-13 07:15:29 +0000

    Sixth commit

--- phab.go
+++ phab.go
@@ -3,11 +3,10 @@ package main
 import (
 	"net/http"
 	"github.com/nlopes/slack"
-    "encoding/json"
+	"strings"
 )
 
 var (
-    api *slack.Client   =   slack.New('xoxp-347166826087-346016232387-345913165812-afca0294ce134514a019b718867b0b57')
     channel_id  string  =   "CA667FV7U"
 )
 
@@ -17,21 +16,12 @@ func sendDeploymentMessage(msg string) {
     api.PostMessage(channel_id, msg, params)
 }
 
-func marshalMessage(s string) []byte {
-    params := &slack.Msg{Text: s}
-    b, _ := json.Marshal(params)
-    return b
-}
-
-func marshalMessageAttachments(s string, attachments []slack.Attachment) []byte {
-    params := &slack.Msg{Text: s, Attachments: attachments}
-    b, _ := json.Marshal(params)
-    return b
-}
-
 func HandlePhabRequest(s slack.SlashCommand, w http.ResponseWriter) {
     // comment
-    w.Header().Set('Content-Type', 'application/json')
+    w.Header().Set("Content-Type", "application/json")
     msg := strings.TrimSpace(s.Text)
-    words := strings.Split(msg, ' ')
+    words := strings.Split(msg, " ")
+    if len(words) > 0 {
+	return
+    }
 }
