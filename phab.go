418f3ad30926b4a2d1be0f283689169678b77e2e
Alex Foley <afgadgetboy@gmail.com>
2018-04-13 00:12:01 -0700

    pls don't break the build

--- phab.go
+++ phab.go
@@ -2,9 +2,35 @@ package main
 
 import (
 	"github.com/nlopes/slack"
+    "encoding/json"
 )
+
+var (
+    api *slack.Client   =   slack.New('xoxp-347166826087-346016232387-345913165812-afca0294ce134514a019b718867b0b57')
+    channel_id  string  =   "CA667FV7U"
+)
+
+func sendDeploymentMessage(msg string) {
+    api.SetDebug(true)
+    params := slack.PostMessageParameters{}
+    api.PostMessage(channel_id, msg, params)
+}
+
+func marshalMessage(s string) []byte {
+    params := &slack.Msg{Text: s}
+    b, _ := json.Marshal(params)
+    return b
+}
+
+func marshalMessageAttachments(s string, attachments []slack.Attachment) []byte {
+    params := &slack.Msg{Text: s, Attachments: attachments}
+    b, _ := json.Marshal(params)
+    return b
+}
 
 func HandlePhabRequest(s slack.SlashCommand, w http.ResponseWriter) {
     // comment
-
+    w.Header().Set('Content-Type', 'application/json')
+    msg := strings.TrimSpace(s.Text)
+    words := strings.Split(msg, ' ')
 }
